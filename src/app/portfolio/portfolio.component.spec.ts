6b104a114c3a5b44bb228134b161c3df770ad961
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-07-22 16:46:25 +0530

    WIP- Create new portfolio section

--- src/app/portfolio/portfolio.component.spec.ts
+++ src/app/portfolio/portfolio.component.spec.ts
@@ -1,16 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { PortfolioComponent } from './portfolio.component';
+import { PortfolioComponent } from "./portfolio.component";
 
-describe('PortfolioComponent', () => {
+describe("PortfolioComponent", () => {
   let component: PortfolioComponent;
   let fixture: ComponentFixture<PortfolioComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PortfolioComponent ]
-    })
-    .compileComponents();
+      declarations: [PortfolioComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('PortfolioComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
