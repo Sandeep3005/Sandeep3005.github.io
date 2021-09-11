4a384397145cf951f2db397d409a146942ef0e7c
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-09-11 15:47:20 +0530

    Add footer file

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/Sandeep3005/Sandeep3005.github.io"
+      className="link footer__link"
+    >
+      Coded with <span>&hearts;</span>
+      by Sandeep Chikhale
+    </a>
+  </footer>
+);
+
+export default Footer;
