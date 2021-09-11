087fc55b9f51edfb97b36cecc46c60e9194c9beb
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2021-09-11 18:01:47 +0530

    Make Header section compatible with typescript

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { header } from "../../portfolio";
+import Navbar from "../Navbar/Navbar";
+import "./Header.css";
+
+interface IHeader {
+  homepage: string;
+  title: string;
+}
+
+const Header = () => {
+  const { homepage, title }: IHeader = header;
+
+  return (
+    <header className="header center">
+      <h3>
+        {homepage ? (
+          <a href={homepage} className="link">
+            {title}
+          </a>
+        ) : (
+          title
+        )}
+      </h3>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
